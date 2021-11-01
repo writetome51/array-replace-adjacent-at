0a47fbc07996bdd36a2aa8e56862660bd8f57c7e
Steve Thompson <writetome51@gmail.com>
2021-11-01 13:34:29 -0600

    Added tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# replaceAdjacentAt(<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;index,<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;newValues: any[],<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;array<br>): void
+# replaceAdjacentAt\<T\>(<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;startingIndex: number,<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;newValues: T[],<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;array: T[]<br>): void
 
-Replaces adjacent items, beginning at `index`, with the same number  
+Replaces adjacent items, beginning at `startingIndex`, with the same number  
 of `newValues`, in `array`.  
 `index` can be negative or positive. 
 
@@ -9,14 +9,25 @@ of `newValues`, in `array`.
 ```js
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-replaceAdjacentAt(-1, [20], arr);  
-// arr is now  [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 20 ]
+replaceAdjacentAt(0, [20], arr);  
+// arr is now  [ 20, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 
-replaceAdjacentAt(0, [20, 21, 22, 23], arr);  
-// arr is now  [ 20, 21, 22, 23, 5, 6, 7, 8, 9, 20 ]
+replaceAdjacentAt(0, [30, 31, 32, 33], arr);  
+// arr is now  [ 30, 31, 32, 33, 5, 6, 7, 8, 9, 10 ]
+
+replaceAdjacentAt(-3, [1, 2, 3], arr);
+// arr is now  [ 30, 31, 32, 33, 5, 6, 7, 1, 2, 3 ]
+
+// This works because the second arg has length greater than zero:
+replaceAdjacentAt(-3, 'aaa', arr);
+// arr is now  [ 30, 31, 32, 33, 5, 6, 7, 'a', 'a', 'a' ]
+
+// This errors because the second arg has length of zero:
+replaceAdjacentAt(0, [], arr);
+// Error: 'Input must be integer greater than zero'
 
 // This errors because arr only has 2 items left at index 8:
-replaceAdjacentAt(8, ['z', 'b', 'c'], arr);
+replaceAdjacentAt(8, ['a', 'b', 'c'], arr);
 // Error: 'the array does not have enough items to fulfill your request'
 ```
 
--- tests.js
+++ tests.js
@@ -1,29 +1,141 @@
 import {replaceAdjacentAt} from './index.js';
+import {isMatch} from '@writetome51/is-match';
 
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-replaceAdjacentAt(1, [10], arr);
-console.log(arr);
+replaceAdjacentAt(0, [10], arr);
+if (isMatch(arr, [10, 2, 3, 4, 5, 6, 7, 8, 9, 10])) console.log('test 1 passed');
+else console.log('test 1 FAILED');
+
 
 replaceAdjacentAt(-1, [20], arr);
-console.log(arr);
+if (isMatch(arr, [10, 2, 3, 4, 5, 6, 7, 8, 9, 20])) console.log('test 2 passed');
+else console.log('test 2 FAILED');
+
 
 replaceAdjacentAt(0, [20, 21, 22, 23], arr);
-console.log(arr);
+if (isMatch(arr, [20, 21, 22, 23, 5, 6, 7, 8, 9, 20])) console.log('test 3 passed');
+else console.log('test 3 FAILED');
+
 
 replaceAdjacentAt(6, ['a', 'b', 'c'], arr);
-console.log(arr);
+if (isMatch(arr, [20, 21, 22, 23, 5, 6, 'a', 'b', 'c', 20])) console.log('test 4 passed');
+else console.log('test 4 FAILED');
 
-replaceAdjacentAt(6, ['z', {prop: 1}, 'c'], arr);
-console.log(arr);
-/*
+
+const obj = {prop: 1};
+replaceAdjacentAt(6, ['z', obj], arr);
+if (isMatch(arr, [20, 21, 22, 23, 5, 6, 'z', obj, 'c', 20])) console.log('test 5 passed');
+else console.log('test 5 FAILED');
+
+
 replaceAdjacentAt(-10, [true, false, true], arr);
-console.log(arr);
+if (isMatch(arr, [true, false, true, 23, 5, 6, 'z', obj, 'c', 20])) console.log('test 6 passed');
+else console.log('test 6 FAILED');
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-replaceAdjacentAt(7, ['z', 'b', 'c'], arr);
-console.log(arr);
-*/
-// This should error:
-// replaceAdjacentAt(8, ['z', 'b', 'c'], arr);
+
+replaceAdjacentAt(0, 'aaa', arr);
+if (isMatch(arr, ['a','a','a', 23, 5, 6, 'z', obj, 'c', 20])) console.log('test 6A passed');
+else console.log('test 6A FAILED');
+
+
+// This should error because arr only has 2 items left at index 8:
+let errorTriggered=false;
+try {
+	replaceAdjacentAt(8, ['a', 'b', 'c'], arr);
+}
+catch {
+	errorTriggered=true;
+}
+if (errorTriggered) console.log('test 7 passed');
+else console.log('test 7 FAILED');
+
+
+errorTriggered=false;
+try {
+	replaceAdjacentAt(-1, ['a', 'b'], arr);
+}
+catch {
+	errorTriggered=true;
+}
+if (errorTriggered) console.log('test 8 passed');
+else console.log('test 8 FAILED');
+
+
+errorTriggered=false;
+try {
+	replaceAdjacentAt(10, ['a', 'b'], arr);
+}
+catch {
+	errorTriggered=true;
+}
+if (errorTriggered) console.log('test 8A passed');
+else console.log('test 8A FAILED');
+
+
+errorTriggered=false;
+try {
+	replaceAdjacentAt('1', ['a', 'b'], arr);
+}
+catch {
+	errorTriggered=true;
+}
+if (errorTriggered) console.log('test 9 passed');
+else console.log('test 9 FAILED');
+
+
+errorTriggered=false;
+try {
+	replaceAdjacentAt('', ['a', 'b'], arr);
+}
+catch {
+	errorTriggered=true;
+}
+if (errorTriggered) console.log('test 10 passed');
+else console.log('test 10 FAILED');
+
+
+errorTriggered=false;
+try {
+	replaceAdjacentAt(false, ['a', 'b'], arr);
+}
+catch (err) {
+	errorTriggered=true;
+}
+if (errorTriggered) console.log('test 11 passed');
+else console.log('test 11 FAILED');
+
+
+errorTriggered=false;
+try {
+	replaceAdjacentAt(0, [], arr);
+}
+catch (err) {
+	errorTriggered=true;
+}
+if (errorTriggered) console.log('test 12 passed');
+else console.log('test 12 FAILED');
+
+
+errorTriggered=false;
+try {
+	replaceAdjacentAt(0, '', arr);
+}
+catch (err) {
+	errorTriggered=true;
+}
+if (errorTriggered) console.log('test 13 passed');
+else console.log('test 13 FAILED');
+
+
+errorTriggered=false;
+try {
+	replaceAdjacentAt(3, ['c'], 'bbbb');
+}
+catch (err) {
+	errorTriggered=true;
+}
+if (errorTriggered) console.log('test 14 passed');
+else console.log('test 14 FAILED');
+
